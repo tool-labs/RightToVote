8fd78586b9d36ac7e66742c7f32ff436300799ae
Robin Krahl <me@robin-krahl.de>
2013-09-13 19:02:03 +0000

    Fork me on GitHub :)

--- tools.inc.php
+++ tools.inc.php
@@ -44,6 +44,7 @@ function print_header($lang,$title)
  <link rel="stylesheet" type="text/css" href="/stimmberechtigung/style.css" />
 </head>
 <body>
+<a href="https://github.com/tool-labs/stimmberechtigung"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
 ');
 }
 
